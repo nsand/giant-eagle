6c0b6c16c7ab7199047220510da92288d8a0bb1a
Nick Sandonato <nick.sandonato@gmail.com>
2017-01-16 21:16:29 -0500

    Make sure to cancel any running timer, and reset the brightness when tapped

--- GE Jobs/SplashController.swift
+++ GE Jobs/SplashController.swift
@@ -11,15 +11,18 @@ import UIKit
 class SplashController : UIViewController {
 
     var brightness: CGFloat = 1;
+    private var timer: Timer?;
 
     override func viewDidLoad() {
         super.viewDidLoad();
         self.brightness = UIScreen.main.brightness;
-        Timer.scheduledTimer(withTimeInterval: 3, repeats: false, block: {(timer) in
+        timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: false, block: {(timer) in
             UIScreen.main.brightness = 0.7;
         });
     }
     @IBAction func startTap(_ sender: UITapGestureRecognizer) {
+        timer?.invalidate();
+        UIScreen.main.brightness = brightness;
         self.performSegue(withIdentifier: "jobApplicationSegue", sender: self);
     }
 }

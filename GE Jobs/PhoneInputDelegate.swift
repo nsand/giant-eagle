7000c40c3112cfa87fd60c82ecd9feee2283e032
Nick Sandonato <nick.sandonato@gmail.com>
2017-01-27 17:58:21 -0500

    Phone number formatting kind of working

--- GE Jobs/PhoneInputDelegate.swift
+++ GE Jobs/PhoneInputDelegate.swift
@@ -14,7 +14,35 @@ class PhoneInputDelegate: NSObject, UITextFieldDelegate {
         if string.characters.count > 0 && Int(string) == nil {
             return false
         }
-        return true
+        let delta = (textField.text! as NSString).replacingCharacters(in: range, with: string)
+        var numbers = delta.components(separatedBy: NSCharacterSet.decimalDigits.inverted)
+
+        var formattedNumber = "1-"
+        var idx = 0
+        if numbers[0] == "1" {
+            numbers.remove(at: 0)
+        }
+        if numbers.count > 2 {
+            formattedNumber.append("(")
+            for subIdx in 0...2 {
+                formattedNumber.append(String(numbers[subIdx]))
+            }
+            formattedNumber.append(")")
+        }
+        else {
+            formattedNumber.append(string)
+        }
+        textField.text = formattedNumber
+        /*var phone = textField.text ?? ""
+        var formattedNumber = ""
+        if phone.characters.count == 0 {
+            formattedNumber = string != "1" ? "1-\(string)" : string
+        }
+        else if phone.characters.count == 4 {
+            formattedNumber = "1-(\(string))"
+        }
+        textField.text = formattedNumber*/
+        return false
     }
 
 }
